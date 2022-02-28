dd0ead1019595a932b7259a5fd0d8561e3ab547d
darkx-11 <crazyaditya11@gmail.com>
2022-02-28 18:23:20 +0530

    ETH-A and final touches

--- scripts/DSA_task.js
+++ scripts/DSA_task.js
@@ -1,5 +1,4 @@
 const hre = require("hardhat");
-//const Web3 = require("web3");
 const { web3 } = require("hardhat");
 const { ethers } = require("hardhat");
 const DSA = require("dsa-connect");
@@ -9,12 +8,6 @@ require("dotenv").config();
 const apiKey = process.env.ethApiKey;
 const PRIVATE_KEY = process.env.PRIVATE_KEY;
 
-// const web3 = new Web3(
-//   new Web3.providers.HttpProvider(
-//     `https://eth-mainnet.alchemyapi.io/v2/${apiKey}`
-//   )
-// );
-
 const dsa = new DSA({
   web3: web3,
   mode: "node",
@@ -22,11 +15,6 @@ const dsa = new DSA({
 });
 
 const my_address = "0x57A8C355790AEe45341f738C8Fb3a35475C86D51";
-
-// network.provider.send("hardhat_setBalance", [
-//   my_address,
-//   ethers.utils.parseEther("10.0").toHexString(),
-// ]);
 
 async function getCurrentGasPrices() {
   let response = await axios.get(
@@ -50,13 +38,6 @@ async function Build() {
     my_address,
     ethers.utils.parseEther("18.0").toHexString(),
   ]);
-  await web3.eth.getBalance(my_address, function (err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(web3.utils.fromWei(result, "ether") + " ETH");
-    }
-  });
   const Prices = await getCurrentGasPrices();
   const Gas = Prices.high * 1000000000;
   console.log(Gas);
@@ -86,36 +67,17 @@ async function Cast() {
     ethers.utils.parseEther("15.0").toHexString(),
   ]);
 
-  await web3.eth.getBalance(newAccountAddress, function (err, result) {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log(web3.utils.fromWei(result, "ether") + " ETH");
-    }
-  });
-
   let spells = await dsa.Spell();
   await spells.add({
     connector: "COMPOUND-A",
     method: "deposit",
     args: [
-      "A",
+      "ETH-A",
       "1000000000000000000", // 1 * 10^18 wei (1 Eth)
       0,
       0,
     ],
   });
-  // await spells.add({
-  //   connector: "COMPOUND-A",
-  //   method: "depositRaw",
-  //   args: [
-  //     "0xEeeeeEeeeEeEeeEeEeEeeEEEeeeeEeeeeeeeEEeE",
-  //     "0x4Ddc2D193948926D02f9B1fE9e1daa0718270ED5",  //cEth on mainnet
-  //     "1000000000000000000", // 1 * 10^18 wei (1 Eth)
-  //     0,
-  //     0,
-  //   ],
-  // });
 
   await spells
     .cast({
